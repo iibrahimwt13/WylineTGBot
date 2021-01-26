4a8490d66a563e10e8f6867816f39e2d12872a79
iibrahimwt13 <63889403+iibrahimwt13@users.noreply.github.com>
2021-01-26 05:32:36 +0300

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -79,7 +79,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT HATA RAPORU**\n"
-                    link = "[FireqanQ Destek Grubu](https://t.me/FireqanQSupport)"
+                    link = "[Wyline Destek Grubu](https://t.me/wylinesupport)"
                     text += "İsterseniz, bunu rapor edebilirsiniz"
                     text += f"- sadece bu mesajı buraya iletin {link}.\n"
                     text += "Hata ve Tarih dışında hiçbir şey kaydedilmez\n"
