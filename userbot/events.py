f634bb62d7571a74af64286ccf5e520072c3af72
iibrahimwt13 <63889403+iibrahimwt13@users.noreply.github.com>
2021-03-26 00:38:07 +0300

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -1,9 +1,4 @@
-# Copyright (C) 2020 FireganqQ
-#
-# Licensed under the GPL-3.0 License;
-# you may not use this file except in compliance with the License.
-#
-# FireqanQUserBot - FireganqQ
+# WylineTGBot - ZeuS
 
 """ Olayları yönetmek için UserBot modülü.
  UserBot'un ana bileşenlerinden biri. """
