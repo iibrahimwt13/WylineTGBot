7f3eac7a23bcc2856cc53dea9ff26144b2e7eb40
iibrahimwt13 <63889403+iibrahimwt13@users.noreply.github.com>
2021-03-26 00:30:59 +0300

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -1,11 +1,3 @@
-# Copyright (C) 2020 FireganqQ
-#
-# Licensed under the GPL-3.0 License;
-# you may not use this file except in compliance with the License.
-#
-# FireqanQUserBot - FireganqQ
-
-""" UserBot başlangıç noktası """
 import importlib
 from importlib import import_module
 from sqlite3 import connect
